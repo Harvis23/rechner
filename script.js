84789ef4e4e5e6e2bd66b26140d0360331e20255
Harvis Castellanos <harvis.castellanos@gmail.com>
2024-05-02 20:29:17 +0200

    Create three global variables

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const botones = document.querySelector(".button-container")
+let num1 = '';
+let num2 = '';
+let operator = '';
+
 const pantalla = document.querySelector(".screen")
 const clear = document.querySelector(".delete")
 const equal = document.querySelector(".equal")
+const decimal = document.querySelector(".decimal")
+let currentValue = '';
+
+let operators = document.querySelectorAll(".operator")
 
 botones.addEventListener("click", event => {
     if (event.target.matches('button')){
         const key = event.target 
         const action = key.dataset.action
-        const keyContent = key.textContent
+        const keyContent = event.target.textContent
         const displayedNum = pantalla.textContent //current displayed number
 
         if (!action)
@@ -35,6 +43,4 @@ botones.addEventListener("click", event => {
 
 })
 
-function operate(num1, num2, operator) {
 
-}
