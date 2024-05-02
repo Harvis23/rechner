029b63066d338d5000a7a8a56e822c54a8013116
Harvis Castellanos <harvis.castellanos@gmail.com>
2024-05-02 17:48:39 +0200

    Add data-action attribute to identify wheter is a key or a number

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const div = document.querySelector(".button-container")
-div.addEventListener("click", event => {
-    const target = event.target
+var num1 = 0;
+var num2 = 0;
+var operator = null;
 
-    if (target.tagName === "BUTTON") {
-        console.log(target.innerText);
-    }
 
-})
+function addition(num1, num2) {
+    display.textContent = num1 + num2;
+    
+}
+
+
+
+function display(num1, num2) {
+    var  pantalla = document.querySelector(".screen")
+    pantalla.textContent = num1.value;
+    pantalla.textContent = num2.value;
+}
+
+
+
+function operate(num1, num2, operator) {
+    const botones = document.querySelector(".button-container")
+    botones.addEventListener("click", event => {
+        numeros = target
+
+
+    
+    })
+
+}
