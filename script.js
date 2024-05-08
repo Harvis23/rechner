d043f9540f37da03d14959113dd40a0cbdc9acd8
Harvis Castellanos <harvis.castellanos@gmail.com>
2024-05-08 11:02:01 +0200

    Add function for the keyboard keys

--- script.js
+++ script.js
@@ -1,57 +1,153 @@
 let previousValue = '';
 let currentValue = '';
-let operator = '';
-let currentDisplay = '0';
+let operator = null;
+
+let currentDisplay  = '0';
 let resultDisplay = false;
 
 let botones = document.querySelector(".button-container")
-let pantalla = document.querySelector(".screen")
-let clear = document.querySelector(".delete")
-let equal = document.querySelector(".equal")
+let clear = document.querySelector(".clear")
 let decimal = document.querySelector(".decimal")
+let equal = document.querySelector(".equal")
 
 let operators = document.querySelectorAll(".operator")
 let numeros = document.querySelectorAll(".operand")
-
-
+ 
+document.addEventListener("keydown", handleKeyboard)
 
 numeros.forEach((numero => {
     numero.addEventListener("click", event => {
         handleNumber(event.target.textContent)
-        display(currentValue);
         
     })
 }))
 
-operators.forEach((operator => {
-    operator.addEventListener("click", event => {
+operators.forEach((op => {
+    op.addEventListener("click", event => {
         handleOperation(event.target.textContent)
     })
 }))
 
+equal.addEventListener("click", () => {
+    handleEqual();
+})
+
+
+clear.addEventListener("click", () => {
+    clearScreen();
+})
+
+decimal.addEventListener("click", ()=> {
+    addDecimal();
+})
+
 function handleNumber(num){
-    if (currentValue.length <= 5){
-        currentValue += num
-    }
+  if (currentValue.length <= 5){
+    currentValue += num;
+    currentDisplay = currentValue;
+  }
 }
 
 function handleOperation(op){
     operator = op;
     previousValue = currentValue;
+    currentDisplay = '';
     currentValue = '';
 }
 
+function handleEqual(){
+    if (operator !== null && previousValue !== '' && currentValue !== ''){
+        const result = operate(previousValue, currentValue, operator);
+        roundNumber(result);
+        display(result);
+        currentValue = '';
+        operator = null
+    }
+}
+
 function display(value){
-    if (currentDisplay === "0" || resultDisplay === false) {
+    if (currentDisplay === "0" || resultDisplay) {
         currentDisplay = value;
     } else {
-        currentDisplay += value;
+        currentDisplay = value;
     }
-    resultDisplay = false; // Flag to track wether the result was displayed
-
+    resultDisplay = false;
     updateDisplay();
 }
 
-function updateDisplay() {
-    pantalla.textContent = currentDisplay;
+function updateDisplay(){
+    let pantalla = document.querySelector(".screen");
+    pantalla.textContent = currentDisplay
+
+}
+
+function operate(previousValue, currentValue, operator){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+    switch (operator){
+        case '+':
+            return addition(previousValue, currentValue);
+        case '-':
+            return substraction(previousValue, currentValue);
+        case '*':
+            return multiplication(previousValue, currentValue);
+        case '/':
+            return division(previousValue, currentValue);
+        default:
+            return 'Error';
+            
+    }
+}
+
+function addition(previousValue, currentValue){
+    return previousValue + currentValue;
+}
+
+function multiplication(previousValue, currentValue){
+    return previousValue * currentValue;
+}
+
+function substraction(previousValue, currentValue){
+    return previousValue - currentValue;
+}
+
+function division(previousValue, currentValue){
+    if (currentValue === 0){
+        return 'Error'
+    }
+    else {
+        return previousValue / currentValue;
+    }
+}
+
+function clearScreen(){
+    previousValue = "";
+    currentValue = "";
+    currentDisplay = "0";
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000
+}
+
+function addDecimal(){
+    if (!currentValue.includes(".")){
+        currentValue += "."
+    }
+}
+
+function handleKeyboard(event) {
+    const key = event.key;
+    if (key >= '0' && key <= '9'){
+        handleNumber(key);
+    }
+    else if (key === '+' || key === '-' || key === '*' || key === '/'){
+        handleOperation(key)
+    }
+    else if (key === '=' || key === 'Enter'){
+        handleEqual();
+    }
+    else if (key === 'Backspace'){
+        clearScreen();
+    }
 }
